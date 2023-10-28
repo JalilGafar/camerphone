9928fdb2fec430d5454263720c2e9dc1489c9545
Fonda Njiemessa Jalil Gafar <fondajalil@hotmail.com>
2023-10-28 06:12:01 +0100

    Camerphone Project initialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {provideClientHydration} from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
